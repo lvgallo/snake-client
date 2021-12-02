c9ab8d81c4d28a0ff34bd1d129d8e45c26c7d471
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2021-12-02 02:48:14 +0000

    play and client for snake game

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");  
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address here,
+    port: 50541,// PORT number here,
+  });
+  conn.setEncoding("utf8");
+  
+  conn.on('connect', () => {
+    console.log("hello");
+    conn.write(`name: LVG`)
+  });
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  
+  // interpret incoming data as text
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {connect}
